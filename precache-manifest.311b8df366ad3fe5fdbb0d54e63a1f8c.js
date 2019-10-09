4a5c0ced029e89e175d9340eaf7ebe35fd1bec21
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-09 20:30:18 +0000

    Deploy KaterinaSoroka/resume to github.com/KaterinaSoroka/resume.git:gh-pages

---
+++ precache-manifest.311b8df366ad3fe5fdbb0d54e63a1f8c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4948808831df600ee93a4473b886ffa5",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "055aef70686575871269",
+    "url": "/resume/static/css/2.f4948e68.chunk.css"
+  },
+  {
+    "revision": "c0d8ad745601c8286496",
+    "url": "/resume/static/css/main.4e6efbae.chunk.css"
+  },
+  {
+    "revision": "055aef70686575871269",
+    "url": "/resume/static/js/2.51aa3fb0.chunk.js"
+  },
+  {
+    "revision": "c0d8ad745601c8286496",
+    "url": "/resume/static/js/main.9df9137b.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "0b81490d5e46208023d61c3731c94243",
+    "url": "/resume/static/media/camping.0b81490d.gif"
+  },
+  {
+    "revision": "ca9ed1a45584f52cd27235343d44c540",
+    "url": "/resume/static/media/programming.ca9ed1a4.jpg"
+  },
+  {
+    "revision": "f066ccf6c48c9572a1477fe2dc672af8",
+    "url": "/resume/static/media/sleeping.f066ccf6.jpg"
+  },
+  {
+    "revision": "3040473464c713c7c0fc12a589f71729",
+    "url": "/resume/static/media/snowboarding.30404734.gif"
+  }
+]);
